38cab88176effa52ee6b4769b9af7fd67bab248a
shaksworld <shakaaliakwe>
2022-06-22 11:38:42 +0100

    second commit

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-// const firstNumber = (parseInt('Enter First Number: '));
-// const operator = ('Enter operator: ');
-// const secondNumber = (parseInt('Enter second Number: '));
-
-// let result;
-
-// if (operator == '+'){
-//     result = firstNumber + secondNumber;
-// }
-// else if (operator == '-'){
-//     result = firstNumber - secondNumber;
-// }
-// else if (operator == '*'){
-//     result = firstNumber * secondNumber;
-// }
-// else {
-//     result = firstNumber / secondNumber;
-// }
-// document.write(1)
-// console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
-
-// let calculator = new calculator() calculator.read
-// let calculator = {
-//     sum() {
-//         return this.a + this.b;
-//     },
-//     mul () {
-//         return this.a * this.b;
-//     },
-
-//     read () {
-//         this.a = +prompt('first', 0)
-//         this.b = +prompt('second', 0)
-//     }
-// }
-let powerCalc = new Calculator;
+const firstNumber = parseFloat(prompt("Please Enter Your First Number: "));
+const operator = prompt("Please Enter Your Value(pick =, -, * or /): ");
+const secondNumber = parseFloat(prompt("Please Enter Your Second Number: "));
+if(operator == '+') {
+    result = firstNumber + secondNumber;
+}
+else if (operator == '-') {
+    result = firstNumber - secondNumber;
+}
+else if (operator == '*') {
+    result = firstNumber * secondNumber;
+}
+else {
+    result = firstNumber / secondNumber
+}
+window.prompt("Your result is " + result)

914906ab1c47d8da3400ff059a5a70417efca4bb
shaksworld <shakaaliakwe>
2022-06-22 11:33:42 +0100

    cal javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+// const firstNumber = (parseInt('Enter First Number: '));
+// const operator = ('Enter operator: ');
+// const secondNumber = (parseInt('Enter second Number: '));
+
+// let result;
+
+// if (operator == '+'){
+//     result = firstNumber + secondNumber;
+// }
+// else if (operator == '-'){
+//     result = firstNumber - secondNumber;
+// }
+// else if (operator == '*'){
+//     result = firstNumber * secondNumber;
+// }
+// else {
+//     result = firstNumber / secondNumber;
+// }
+// document.write(1)
+// console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
+
+// let calculator = new calculator() calculator.read
+// let calculator = {
+//     sum() {
+//         return this.a + this.b;
+//     },
+//     mul () {
+//         return this.a * this.b;
+//     },
+
+//     read () {
+//         this.a = +prompt('first', 0)
+//         this.b = +prompt('second', 0)
+//     }
+// }
+let powerCalc = new Calculator;
